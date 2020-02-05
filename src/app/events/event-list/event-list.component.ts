feb5b0a77887fa5dffe8ca8398321221bc110629
David Long <davdaarn@users.noreply.github.com>
2020-02-05 07:25:28 -0700

    renamed some things

--- src/app/events/event-list/event-list.component.ts
+++ src/app/events/event-list/event-list.component.ts
@@ -15,7 +15,7 @@ export class EventListComponent implements OnInit {
     date: '9/26/2036',
     time: '10:00 am',
     price: 599.99,
-    imageUrl: './assets/images/angularconnect-shield.png',
+    imageUrl: '../../../assets/images/angularconnect-shield.png',
     location: {
       address: '1057',
       city: 'London',
--- src/app/events/event-thumbnail/event-thumbnail.component.spec.ts
+++ src/app/events/event-thumbnail/event-thumbnail.component.spec.ts
@@ -1,6 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { EventThumbnailComponent } from './events-thumbnail.component';
+import { EventThumbnailComponent } from './event-thumbnail.component';
 
 describe('EventsThumbnailComponent', () => {
   let component: EventThumbnailComponent;
